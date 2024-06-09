1b26387aa47b8c0e08b4370d743e92249b9dae45
Germán González <germangonzalez@germans-macbook.home>
2024-06-09 22:44:02 +0200

    Se sube script para cargar archivos a SFTP

---
+++ upload-sftp/pythonProject/main.py
@@ -0,0 +1,15 @@
+import paramiko
+from paramiko import SSHClient
+
+# Store Variables
+localPath = '/Users/germangonzalez/Documents/Repositorios/python-projects/upload-sftp/pythonProject/test.txt'
+remotePath = '/sftp/test.txt'
+
+# Configure Client
+client = SSHClient()
+client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
+client.connect('localhost', 23, 'demo', 'demo')
+
+# Execute SFTP Commands
+sftp = client.open_sftp()
+sftp.put(localPath, remotePath)
